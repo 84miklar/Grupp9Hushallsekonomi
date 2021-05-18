709947b7162ee177db7964dd643750baa193e400
Mikael Larsson <mikael_larsson84@hotmail.com>
2021-05-18 10:54:13 +0200

    XML-kommenterat Savings.

--- Grupp9Hushallsekonomi/Account/Savings.cs
+++ Grupp9Hushallsekonomi/Account/Savings.cs
@@ -5,53 +5,73 @@ using System.Text;
 
 namespace Grupp9Hushallsekonomi.Account
 {
+    /// <summary>
+    /// Class to handle all the savings.
+    /// </summary>
     public class Savings
     {
-        public double totalSavings = 0;
+        /// <summary>
+        /// Savings maximum value possible.
+        /// </summary>
+        public const double maxPercentage =1.0;
 
-        public const double MaxPercentage = 1;
+        /// <summary>
+        /// The savings percentage to set.
+        /// </summary>
         public double SavingsPercantage { get; set; }
+
+        /// <summary>
+        /// The name of the saving.
+        /// </summary>
         public string Name { get; set; }
+
         public Savings(string name, double percantage)
         {
             Name = name;
             SavingsPercantage = percantage;
         }
-        public Savings(){}
+        public Savings() { }
+
+        /// <summary>
+        /// Turns the percentage value of a saving into money value.
+        /// </summary>
+        /// <param name="income"></param>
+        /// <returns>The savings value in double.</returns>
         public double CalculatePercentageToMoney(double income)
         {
-            var actualPrecentage = MaxPercentage - SavingsPercantage;
+            var actualPrecentage = maxPercentage - SavingsPercantage;
             return income > 0 ? income * actualPrecentage : 0;
-          
         }
+
+        /// <summary>
+        /// Reduces the savings from the income.
+        /// </summary>
+        /// <param name="income"></param>
+        /// <returns>Income - saving in double.</returns>
         public double SumLeftAfterSaving(double income)
         {
-           return income > 0 ? income - CalculatePercentageToMoney(income) : 0;
-            //if (income > 0)
-            //{
-
-            //    return income - CalculatePercentageToMoney(income);
-            //}
-            //return 0;
+            return income > 0 ? income - CalculatePercentageToMoney(income) : 0;
         }
+
+        /// <summary>
+        /// Checks if income is more than zero and saving is possible.
+        /// </summary>
+        /// <param name="income"></param>
+        /// <returns>True if possible</returns>
         public bool IsSavingPossible(double income)
         {
             return income > 0 ? CheckSumAfterSavingAndSavingsPercentage(income) : false;
-            //if (income > 0)
-            //{
-            //    return CheckSumAfterSavingAndSavingsPercentage(income);
-            //    //if (sumAfterSaving >= 0 && SavingsPercantage <= MaxPercentage)
-            //    //{
-            //    //    return true;
-            //    //}
-            //}
-            //return false;
         }
 
+        /// <summary>
+        /// Checks if a saving is possible to do.
+        /// </summary>
+        /// <param name="income"></param>
+        /// <returns>True if sum of income left after saving is done is more then 0.</returns>
         private bool CheckSumAfterSavingAndSavingsPercentage(double income)
         {
             var sumAfterSaving = SumLeftAfterSaving(income);
-            return sumAfterSaving >= 0 && SavingsPercantage <= MaxPercentage ? true : false;
+            return sumAfterSaving >= 0 && SavingsPercantage <= maxPercentage ? true : false;
         }
 
         /// <summary>
@@ -62,7 +82,7 @@ namespace Grupp9Hushallsekonomi.Account
         /// False if list is null</returns>
         public bool CheckSavings(List<Savings> savingsList)
         {
-            
+            double totalSavings = 0;
             var log = new Logger();
             var moneyLeft = BudgetCalculator.totalIncome.Money;
             if (moneyLeft > 0 && savingsList != null)
